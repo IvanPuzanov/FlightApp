7fa04814fb70b84242ccc4a1f4a2ad786bcdad18
Ivan Puzanov <ivanpuzan0v.dev@gmail.com>
2026-06-28 18:06:42 +0300

    Initial Commit

---
+++ flight-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flight-demo
+//
+//  Created by Ivan Puzanov on 28.06.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
